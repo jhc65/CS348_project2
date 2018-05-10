5cb0ede9b35bb33115fb67a32d202f575ef2eff2
Joe <jhc65@drexel.edu>
2018-05-09 22:12:01 -0400

    - uh. new scene (sorry Sam) - empty Scripts - attached scripts to things. and stuff

---
+++ Whack-a-Word/Assets/Scripts/Mole.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Mole : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
