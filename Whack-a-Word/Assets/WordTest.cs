5ea588bab7535bd1f8cb9687101ec38a29dc1a47
SlightPlus <nero.DDX@gmail.com>
2018-05-16 11:59:21 -0400

    > Added UI Framework into main.unity.

---
+++ Whack-a-Word/Assets/WordTest.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class WordTest : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		InvokeRepeating("WordSpew", 0, 1.0f);
+	}
+	
+	void WordSpew() {
+        Debug.Log("Letter?: " + GetRandomLetter());
+    }
+
+    char GetRandomLetter() {
+        char kickback = (char) Random.Range(65,90);
+        return kickback;
+    }
+}
